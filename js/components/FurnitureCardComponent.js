fa1868e4d28a3676e78f49844bbfd424d849c26d
Greta Aksamitauskaite <atataite@gmail.com>
2021-11-15 17:09:33 +0200

    8: kortelių atvaizdavimas su savybėmis

--- js/components/FurnitureCardComponent.js
+++ js/components/FurnitureCardComponent.js
@@ -6,10 +6,45 @@ class FurnitureCardComponent {
     }
 
     init = () => {
+        const {title,type,price,location,owner,imgSrc} = this.props;
+        const {country,city,street} = location;
+        const {fullname,mobile,address,email} = owner;
+
+
         this.htmlElement = document.createElement('article');
-        this.htmlElement.className = ' card p-4 shadow';
+        this.htmlElement.className = ' card p-2 shadow';
         this.htmlElement.innerHTML= `
-        <h2 class="h5">Korta</h2>
+        <div class="card" >
+        <img src="${imgSrc}" class="card-img-top">
+        <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">${price} €</p>
+        </div>
+        <div class= "p-2">
+            <div class="fw-light">${type}</div>
+            <div >
+            <div><strong>Rasite adresu:</strong></div>
+            <ul>
+            <li >${country}</li>
+            <li >${city}</li>
+            <li >${street}</li>
+            </ul>
+            </div>
+            <hr>
+            <div >
+            <div><strong>Savininkas:</strong></div>
+            <ul>
+            <li >${fullname}</li>
+            <li >${mobile}</li>
+            <li >${address}</li>
+            <li >${email}</li>
+            </ul>
+            </div>
+        </div>
+    <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+        </div>
+    </div>
         `;
 
     }
